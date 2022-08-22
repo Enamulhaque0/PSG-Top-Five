fa2984a71fca33beb34b3a273744825b4cc6e823
Enamulhaque0e <enamulhaq243@gmail.com>
2022-08-22 13:07:55 +0600

    display player name function added

--- js/app.js
+++ js/app.js
@@ -8,7 +8,45 @@ const playerCart =[]
 
 
 
+const Calculate=document.getElementById("Calculate-btn").addEventListener("onClik")
 
+
+
+
+
+
+
+
+
+
+
+function displayPalayerName(){
+
+const playerContainer =document.getElementById("player-container")
+playerContainer.textContent=""
+
+for(let i = 0; i<playerCart.length; i++){
+if(i <= 4){
+
+    const div = document.createElement("div");
+    div.innerHTML=
+
+    `
+    <ol class=" flex my-6">
+      <li class="ms-4">${i+1}</li>
+      <li class="mx-6">${playerCart[i].playerName}</li>
+    </ol>`
+            
+            playerContainer.appendChild(div)
+}
+
+else{
+    alert("invalid")
+}
+    
+}
+
+}
 
 
 
