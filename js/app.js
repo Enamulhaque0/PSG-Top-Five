e9e43fbab485cc3b564bdccb04e9b462373287ca
Enamulhaque0e <enamulhaq243@gmail.com>
2022-08-22 23:52:09 +0600

    select only five validition added

--- js/app.js
+++ js/app.js
@@ -1,69 +1,41 @@
+const playerCart = [];
 
-const playerCart =[]
-  
+function displayPalayerName() {
+  const playerCount = document.getElementById("seleted-player");
+  playerCount.innerText = playerCart.length;
 
+  const playerContainer = document.getElementById("player-container");
+  playerContainer.textContent = "";
 
- 
+  for (let i = 0; i < playerCart.length; i++) {
+    if (i <= 4) {
+      const div = document.createElement("div");
+      div.innerHTML = `
+            <ol class=" flex my-6">
+              <li class="ms-4">${i + 1}</li>
+              <li class="mx-6">${playerCart[i].playerName}</li>
+            </ol>`;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function displayPalayerName(){
-
-const playerContainer =document.getElementById("player-container")
-playerContainer.textContent=""
-
-for(let i = 0; i<playerCart.length; i++){
-if(i <= 4){
-
-    const div = document.createElement("div");
-    div.innerHTML=
-
-    `
-    <ol class=" flex my-6">
-      <li class="ms-4">${i+1}</li>
-      <li class="mx-6">${playerCart[i].playerName}</li>
-    </ol>`
-            
-            playerContainer.appendChild(div)
-}
-
-else{
-    alert(" Hey Buddy ---You Can Select Only Five Players")
-}
-    
-}
-
-}
-
-
-
-
-
-function selectToCart (element) {
-
-    const playerName=element.parentNode.parentNode.children[0].children[0].innerText;
-    const name= {
-
-        playerName : playerName
+      playerContainer.appendChild(div);
+    } else {
+      return;
     }
-
-    playerCart.push(name)
-
-    displayPalayerName()
-
+  }
 }
 
+function selectToCart(element) {
+  if (playerCart.length <= 4) {
+    const playerName =
+      element.parentNode.parentNode.children[0].children[0].innerText;
+    const name = {
+      playerName: playerName,
+    };
 
+    playerCart.push(name);
+
+    displayPalayerName();
+    element.disabled = true;
+  } else {
+    alert(" Hey Buddy ---You Can Select Only Five Players");
+  }
+}
